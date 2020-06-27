3bccba600d7d82d39d76d518696910a74193d175
waisal.riyanto <waisalriyanto.rpl2.16@gmail.com>
2020-06-27 14:37:30 +0700

    Updates

---
+++ precache-manifest.ca112b32e5ab9805c4e5de5cb8d33dab.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bdfd521fa5fec3e62868f8a9bfde09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0eb013fc73316b5f1a6",
+    "url": "/static/css/2.cc73468a.chunk.css"
+  },
+  {
+    "revision": "a739a975be5db69444b5",
+    "url": "/static/css/main.680f19f9.chunk.css"
+  },
+  {
+    "revision": "d0eb013fc73316b5f1a6",
+    "url": "/static/js/2.a91c2b1f.chunk.js"
+  },
+  {
+    "revision": "5403b55c1cb422a72be68a013c0b9049",
+    "url": "/static/js/2.a91c2b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a739a975be5db69444b5",
+    "url": "/static/js/main.f050e160.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
